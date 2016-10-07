37bff77acb8761347437f278db8d1dc246e45786
Haruka Matsumoto <harukam0416@gmail.com>
2016-10-07 23:26:42 +0900

    'Coin Charge' straight-forward 1st.

---
+++ coin.cc
@@ -0,0 +1,29 @@
+// 'Coin Charge'
+// 2016-10-06
+#include <iostream>
+#include <vector>
+
+int coins[] = {1, 5, 10, 100, 500};
+int size = 5;
+
+int calc(int m, int i) {
+  if(m == 0)
+    return 1;
+  if(i < 0 || size <= i)
+    return 0;
+
+  int x = 0;
+  int coin = coins[i];
+  for(int j = 0;; j++) {
+    int rest = m - (coin * j);
+	if(rest < 0) break;
+    x += calc(rest, i + 1);
+  }
+  return x;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  std::cout << calc(n, 0) << "\n";
+}
