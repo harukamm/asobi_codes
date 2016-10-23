0dc71c4da46e26e235402b5a0e6996ffb09e0278
Haruka Matsumoto <harukam0416@gmail.com>
2016-10-24 00:33:21 +0900

    'Osoi check bst'

--- tree.ml
+++ tree.ml
@@ -4,7 +4,31 @@
 type 'a tree_t = Empty
                | Node of 'a tree_t * 'a * 'a tree_t
 
-type order = Desc (* right-tree < val < left-tree *)
-	   | Asc  (* left-tree < val < right-tree *)
+type order = Desc (* r < v < l *)
+	   | Asc  (* l < v < r *)
+type pos = Left
+	 | Right
 
+let tree1 = Node (Empty, "a", Empty)
+let tree2 = Node (tree1, "b", Node (Empty, "c", Empty))
+let tree3 = Node (tree2, "b", Empty)
+
+(* check_bst : 'a tree -> bool *)
+let check_bst tree =
+  let rec getlst tr =
+    match tr with (* flatten *)
+      Empty -> []
+    | Node (l, v, r) ->
+       let (lstl, lstr) = (getlst l, getlst r) in
+       if lstl = [] then v :: lstr
+       else List.append lstl (v :: lstr)
+  in
+  let lst = getlst tree in
+  let sorted1 = List.sort (fun a b -> if a < b then -1 else 1) lst in
+  let sorted2 = List.sort (fun _ _ -> -1) sorted1 in (* List.rev *)
+  lst = sorted1 || lst = sorted2
+
+let test1 = check_bst tree1 = true
+let test2 = check_bst tree2 = true
+let test3 = check_bst tree3 = false
 
