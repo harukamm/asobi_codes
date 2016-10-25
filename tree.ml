7ef00a51a3c4167a2358114e27be45d331ee95ff
Haruka Matsumoto <harukam0416@gmail.com>
2016-10-25 23:55:01 +0900

    'Another check bst.'

--- tree.ml
+++ tree.ml
@@ -4,15 +4,11 @@
 type 'a tree_t = Empty
                | Node of 'a tree_t * 'a * 'a tree_t
 
-type order = Desc (* r < v < l *)
-	   | Asc  (* l < v < r *)
-type pos = Left
-	 | Right
-
 let tree1 = Node (Empty, "a", Empty)
 let tree2 = Node (tree1, "b", Node (Empty, "c", Empty))
 let tree3 = Node (tree2, "b", Empty)
 
+(* kuso *)
 (* check_bst : 'a tree -> bool *)
 let check_bst tree =
   let rec getlst tr =
@@ -32,3 +28,58 @@ let test1 = check_bst tree1 = true
 let test2 = check_bst tree2 = true
 let test3 = check_bst tree3 = false
 
+type order = Desc1 (* max (r) <= v < min (l) *)
+           | Desc2 (* max (r) < v <= min (l) *)
+           | Asc1  (* max (l) <= v < min (r) *)
+           | Asc2  (* max (l) < v <= min (r) *)
+
+let less = fun x y -> x < y
+let eqless = fun x y -> x <= y
+
+(* fumei *)
+(* check_bst2 : 'a tree_t -> bool *)
+let check_bst2 tree =
+  (* calc : ('a option * 'a option) -> int ->
+            ('a option * 'a option) option -> order -> ('a option * 'a option) *)
+  let calc l v r o = match (o, l, r) with
+    | (Desc1, (min_l, _), (_, max_r))
+    | (Desc2, (min_l, _), (_, max_r)) ->
+       let (op1, op2) = if o = Desc1 then (eqless, less) else (less, eqless) in
+       begin
+	 match (min_l, max_r) with
+	   (None, None) -> (Some v, Some v)
+	 | (None, Some (maxr)) when op1 maxr v -> (Some maxr, Some v)
+	 | (Some (minl), None) when op2 v minl -> (Some v, Some minl)
+	 | (Some (minl), Some (maxr)) when op1 maxr v && op2 v minl -> (Some maxr, Some minl)
+	 | _ -> raise Not_found
+       end
+    | (Asc1,  (_, max_l), (min_r, _))
+    | (Asc2,  (_, max_l), (min_r, _)) ->
+       let (op1, op2) = if o = Asc1 then (eqless, less) else (less, eqless) in
+       begin
+	 match (max_l, min_r) with
+	   (None, None) -> (Some v, Some v)
+	 | (None, Some (minr)) when op2 v minr -> (Some v, Some minr)
+	 | (Some (maxl), None) when op1 maxl v -> (Some maxl, Some v)
+	 | (Some (maxl), Some (minr)) when op1 maxl v && op2 v minr -> (Some maxl, Some minr)
+	 | _ -> raise Not_found
+       end
+  in
+  (* inner : 'a tree_t -> Desc -> ('a option * 'a option) *)
+  let rec inner tr order =
+    match tr with
+      Empty -> (None, None)
+    | Node (l, v, r) -> calc (inner l order) v (inner r order) order
+  in
+  let try1 f =
+    try let _ = f () in true
+    with Not_found -> false   (* not bst. *)
+  in
+  try1 (fun () -> inner tree Desc1)
+  || try1 (fun () -> inner tree Asc1)
+  || try1 (fun () -> inner tree Desc2)
+  || try1 (fun () -> inner tree Asc2)
+
+let test1 = check_bst2 tree1 = true
+let test2 = check_bst2 tree2 = true
+let test3 = check_bst2 tree3 = false
