34aace87e5e7645a27dbf7214854a1dfc429cac9
Haruka Matsumoto <harukam0416@gmail.com>
2016-11-03 01:04:50 +0900

    reverse.

--- uniqc.cc
+++ uniqc.cc
@@ -23,9 +23,29 @@ bool isUnique2(std::string str) {
   return true;
 }
 
+std::string reverse(std::string s) {
+  int n = s.size();
+  std::string str = s;
+  for(int i = 0, j = n - 1; i < j; i++, j--) {
+    char tmp = str[i];
+    str[i] = str[j];
+    str[j] = tmp;
+  }
+  return str;
+}
+
+std::string reverse2(std::string s) {
+  int n = s.size();
+  if(n < 2) return s;
+  std::string h(1, s[0]);
+  std::string t = reverse2(s.substr(1));
+  return t + h;
+}
+
 int main() {
   std::string str;
   std::cin >> str;
   std::cout << str << " is unique " << isUnique2(str) << "\n";
+  std::cout << str << " reversed " << reverse2(str) << "\n";
   return 0;
 }
