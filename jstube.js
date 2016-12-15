af7a5607e52ca633b83e1c510990372adf14db55
Haruka Matsumoto <harukam0416@gmail.com>
2016-12-15 22:52:53 +0900

    httprequest for jstube

--- jstube.js
+++ jstube.js
@@ -2,11 +2,12 @@
 // Respects: https://github.com/NFicano/pytube
 
 function Tube(opt_url) {
-    this.filename_ = "";
-    this.video_url_ = "";
-    this.videos_ = [];
-    if(opt_url)
-	this.from_url_(opt_url);
+  this.filename_ = "";
+  this.video_url_ = "";
+  this.title_ = "";
+  this.videos_ = [];
+  if(opt_url)
+	  this.from_url_(opt_url);
 };
 
 Tube.prototype.url = function() {
@@ -18,4 +19,28 @@ Tube.prototype.url = function(url) {
 };
 
 Tube.prototype.from_url_ = function(url) {
+  this.vide_url = url;
+  this.filename_ = "";
+  this.videos_ = [];
+
+  var video_data = this.get_video_data_();
+};
+
+Tube.prototype.get_video_data_ = function() {
+  this.title_ = "";
+  var url = this.url();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if(this.readyState == 4) {
+      if(xhr.status == 200) {
+        // success
+      } else {
+        console.error("failed");
+      }
+    } else {
+      console.error("cannot open " + url);
+    }
+  };
+  xhr.open('GET', url, true);
+  xhr.send(null);
 };
