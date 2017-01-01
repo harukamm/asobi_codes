e56e245058c4ceb89e42ad44738ad5d4c4a965d5
Haruka Matsumoto <harukam0416@gmail.com>
2017-01-01 23:48:42 +0900

    defined 2 interpreters for lazyk

--- lazyk.hs
+++ lazyk.hs
@@ -1,23 +1,7 @@
-import Prelude hiding (return, (>=))
-import Parser hiding (expr, expr')
-
+-- LazyK interpreter
 data Expr = App Expr Expr | I | K | S deriving Show
 
-{-
-  isk := "i" | "s" | "k"
-  expr := isk | "`" expr expr | expr'
-  expr' := "(" expr ")"
--}
-
-i :: a -> a
-i = \x -> x
-
-k :: a -> b -> a
-k = \x -> \y -> x
-
-s :: (a -> b -> c) -> (a -> b) -> a -> c
-s = \x -> \y -> \z -> (x z) (y z)
-
+-- interpreter
 apply :: Expr -> Expr -> Expr
 apply I x = x
 apply (App K x) y = x
@@ -28,31 +12,75 @@ eval :: Expr -> Expr
 eval (App x y) = apply (eval x) (eval y)
 eval x = x
 
-isk :: Parser Expr
-isk = (symbol "i" >= const (return I))
-      +++
-      (symbol "s" >= const (return S))
-      +++
-      (symbol "k" >= const (return K))
+{- 1. expr := 'i' | 's' | 'k' | '`' expr expr -}
+expr :: String -> [(Expr, String)]
+expr ('`' : s1) = case (expr s1) of
+                   [] -> []
+                   [(e1, s1)] ->
+                       case (expr s1) of
+                         [] -> []
+                         [(e2, s2)] -> [(App e1 e2, s2)]
+expr ('i' : s) = [(I, s)]
+expr ('s' : s) = [(S, s)]
+expr ('k' : s) = [(K, s)]
+expr _         = []
 
-expr :: Parser Expr
-expr = expr'
-       +++
-       isk
-       +++
-       (symbol "`" >= const
-        expr >= \e1 ->
-        expr >= \e2 ->
-        return (App e1 e2))
+{- 2. isk := 'I' | 'S' | 'K'
+      expr' := isk | expr' isk | '(' expr' ')' -}
+many :: (String -> [(Expr, String)]) -> String -> [([Expr], String)]
+many p s = case (p s) of
+             [] -> [([], s)]
+             [(e, ss)] ->
+                      case (many p ss) of
+                        [] -> [([e], ss)]
+                        [(es, s')] -> [(e : es, s')]
 
-expr' :: Parser Expr
-expr' = symbol "(" >= const
-        expr >= \e ->
-        symbol ")" >= const
-        (return e)
+expr_h' :: String -> [(Expr, String)]
+expr_h' ('(' : s) = case (expr' s) of
+                      [(e, ')' : ss)] -> [(e, ss)]
+                      _ -> []
+expr_h' ('I' : s) = [(I, s)]
+expr_h' ('S' : s) = [(S, s)]
+expr_h' ('K' : s) = [(K, s)]
+expr_h' _         = []
 
-e1 :: Expr
-e1 = fst (head (parse expr "`k``s``si`k```s``sss```s``s`ks`ssi``ss`ki``s`ksk`k``s``si`k```ss``s``ss`ki``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss``ss``s``sss``s``sss``ss`ki``s`ksk`k``s``si`k```s``ss```ssi``ss`ki``ss`ki``s`ksk`k``s``si`k```s``si``ss``s``sss``ss`ki``ss```ss``ssi``ss`ki``s`ksk`k``s``si`k```ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss``ss``ss``ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```s``ss`ki``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss```ss`ss``ss`ki``s`ksk`k`k```sii```sii``s``s`kski"))
+expr' :: String -> [(Expr, String)]
+expr' s = case (many expr_h' s) of
+            [(e : elst, ss)] -> [(exp, ss)]
+                where exp = foldl (\t -> \ex -> App t ex) e elst
+            _ -> []
+{- expr_h' と expr' の定義を一つにまとめると、左結合性が保たれず、
+   結合性がごっちゃになるのでだめ。例えば、SII が S(II) になっちゃう -}
 
-main = print e1 >>= const
-       (print (eval e1))
+-- iriguchi
+lazyk1 :: String -> Expr
+lazyk1 s = case (expr s) of
+             [] -> error ""
+             [(e, [])] -> e
+             [(e, s)] -> error s
+
+lazyk2 :: String -> Expr
+lazyk2 s = case (expr' s) of
+            [] -> error ""
+            [(e, [])] -> e
+            [(e, s)] -> error s
+
+hw :: String
+hw = "`k``s``si`k```s``sss```s``s`ks`ssi``ss`ki``s`ksk`k``s``si`k```ss``s``ss`ki``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss``ss``s``sss``s``sss``ss`ki``s`ksk`k``s``si`k```s``ss```ssi``ss`ki``ss`ki``s`ksk`k``s``si`k```s``si``ss``s``sss``ss`ki``ss```ss``ssi``ss`ki``s`ksk`k``s``si`k```ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss``ss``ss``ss``s``sss``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```s``si``ss``ss`ki```ss`s``sss``ss`ki``s`ksk`k``s``si`k```s``ss`ki``ss```ss`ss``ss`ki``s`ksk`k``s``si`k```ss```ss`ss``ss`ki``s`ksk`k`k```sii```sii``s``s`kski"
+
+hw_expr :: Expr
+hw_expr = lazyk1 hw
+
+hw_expr' :: Expr
+hw_expr' = eval hw_expr
+
+ch256 :: String
+ch256 = "SII(SII(S(S(KS)K)I))"
+
+ch256_expr :: Expr
+ch256_expr = lazyk2 ch256
+
+ch256_expr' :: Expr
+ch256_expr' = eval ch256_expr
+
+main = print ch256_expr >>= const (print ch256_expr')
