f839e336e2e7973742f2fa1bac0681e21e539422
Haruka Matsumoto <harukam0416@gmail.com>
2017-01-09 22:39:49 +0900

    added class parser

--- jvm/JVM.java
+++ jvm/JVM.java
@@ -4,40 +4,66 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.*;
 
 public class JVM {
-    static String fname = "Hello.class";
+    private String fname;
+    private boolean state;
 
-    static boolean isClassFile(String fname) {
-	boolean state = false;
+    private int[] data;
+    private int len;
+    private int ptr;
+
+    public JVM(String fname) {
+	this.fname = fname;
+	this.ptr = 0;
+	this.state = false;
 
 	try {
-	    File file = new File(fname);
-	    FileReader fr = new FileReader(file);
-
-	    int n = -1;
-	    long target = 0xcafebabeL;
-	    long s = 0;
-	    for(int i = 0; (n = fr.read()) != -1 && i < 4; i++) {
-		s = (s << 8) | n ;
+	    Path src = Paths.get(fname);
+	    byte[] tmp = Files.readAllBytes(src);
+	    this.len = tmp.length;
+	    int[] data = new int[tmp.length];
+	    for(int i = 0; i < tmp.length; i++) {
+		int val = 0xFF & tmp[i];
+		data[i] = val;
 	    }
-	    fr.close();
-	    if(s == target) {
-		state = true;
-	    }
-	} catch(FileNotFoundException e) {
+	    this.data = data;
+	    this.state = true;
+	} catch(OutOfMemoryError e) {
 	    System.out.println(e);
 	} catch(IOException e) {
 	    System.out.println(e);
 	}
-	return state;
+    }
+
+    private int readU1() throws IOException {
+	int nptr = this.ptr + 1;
+	int val = 0;
+	if(this.len <= nptr) throw new IOException();
+	val = this.data[nptr];
+	this.ptr = nptr;
+
+	return val;
+    }
+
+    private int readU2() throws IOException {
+    }
+
+    private int readU4() throws IOException {
+    }
+
+    public boolean classParser() {
+	boolean success = false;
+	return success;
     }
 
     public static void main(String[] args) {
-	if(isClassFile(fname)) {
-	    System.out.println("yes ^.^");
+	JVM jvm = new JVM("Hello.class");
+	if(jvm.classParser()) {
+	    // System.out.println("yes ^.^");
 	} else {
-	    System.out.println("no  ;_;");
+	    System.out.println("failed classParser");
 	}
     }
 }
