8de64353418ada10026cb8c5a0e1946dd8e63993
Haruka Matsumoto <harukam0416@gmail.com>
2017-02-19 19:30:01 +0900

    Added first rule3 for ll

--- ll.hs
+++ ll.hs
@@ -15,6 +15,9 @@ unduplicate :: Eq a => [a] -> [a]
 unduplicate []       = []
 unduplicate (x : xs) = opcons x (unduplicate xs)
 
+all_codomain :: Sym -> Grammer -> [[Sym]]
+all_codomain x = foldl (\l (y, w) -> if x == y then w : l else l) []
+
 append_set :: Eq a => [a] -> [a] -> [a]
 append_set [] l2       = l2
 append_set (x : xs) l2 = append_set xs (opcons x l2)
@@ -32,6 +35,14 @@ update_f (t, set) ((t', set') : fs) = if t == t'
                                       then update_f (t, append_set set' set) fs
                                       else (t', set') : update_f (t, set) fs
 
+exist :: [Sym] -> [First_t] -> Bool
+exist t []             = False
+exist t ((t', _) : fs) = (t == t') || (exist t fs)
+
+assoc :: [Sym] -> [First_t] -> First_t
+assoc t []               = error "not_found"
+assoc t ((t', set) : fs) = if t == t' then (t', set) else assoc t fs
+
 has_null :: [Sym] -> Bool
 has_null = any (Null==)
 
@@ -46,6 +57,13 @@ fs_rule2 g fs ([x], set) = (f, fs)
           f = ([x], set')
 fs_rule2 _ fs f          = (f, fs)
 
+fs_rule3 :: FsRule
+fs_rule3 g fs ([x], set) = (f, fs)
+    where cs = all_codomain x g
+          cfs = map (\w -> if exist w fs then assoc w fs else first g fs w) cs
+          f = add_all_fs cfs ([x], set)
+fs_rule3 _ fs f          = (f, fs)
+
 app_rule :: FsRule -> Grammer -> [First_t] -> First_t -> [First_t]
 app_rule r g fs f = update_f f' fs2
     where (f', fs2) = r g fs f
@@ -57,11 +75,17 @@ map_app_rule r g fs = h fs fs
           h (f : fs) fs' = h fs (app_rule r g fs' f)
 
 fs_rules :: [FsRule]
-fs_rules = [fs_rule1, fs_rule2]
+fs_rules = [fs_rule1, fs_rule2, fs_rule3, fs_rule3, fs_rule3]
 
 apps :: [FsRule] -> Grammer -> [First_t] -> [First_t]
 apps []       g fs = fs
 apps (r : rs) g fs = apps rs g (map_app_rule r g fs)
+
+first :: Grammer -> [First_t] -> [Sym] -> First_t
+first g fs []             = ([], [])
+first g fs (Null : xs)    = (Null : xs, [])
+first g fs (Term x : xs)  = (Term x : xs, [Term x])
+first g fs (NTerm x : xs) = (NTerm x : xs, [])
 
 make_firsts :: Grammer -> [First_t]
 make_firsts g = map (\t -> assoc [t] fs') ts
